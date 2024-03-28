eeebb7fd59aa12eb88dada83b98da2de13860591
karthik1220 <karthikgithub1102@gmail.com>
2024-03-28 11:15:29 +0530

    initial commit

--- notifications/build.gradle.kts
+++ notifications/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.kartheek.datastore"
+    namespace = "com.kartheek.notifications"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.kartheek.datastore"
+        applicationId = "com.kartheek.notifications"
         minSdk = 26
         targetSdk = 33
         versionCode = 1
@@ -53,19 +53,12 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-
-
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-
-    //datastore
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
